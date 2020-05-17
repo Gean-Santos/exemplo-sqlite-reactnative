df9e1ceba6ada6b33ee0b703b844661a13ff0d06
Gean-Santos <geansantos_15@hotmail.com>
2020-05-16 23:41:25 -0300

    Changing App

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import databaseInit from './src/database/init-database';
+
+import Home from './src/pages/Home';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    new databaseInit;
+    console.log("initialize database");
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Home />
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
