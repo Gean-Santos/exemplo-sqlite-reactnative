779c9f3ef3d9069fa3e2b20172ef7159a2ebc9ca
Gean-Santos <geansantos_15@hotmail.com>
2020-05-17 04:10:35 -0300

    Editing component Pessoa

--- src/components/Pessoa/index.js
+++ src/components/Pessoa/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 const Pessoa = props => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{props.id} - </Text>
-      <Text style={styles.text}>{props.nome} - </Text>
-      <Text style={styles.text}>{props.telefone} - </Text>
-    </View>
+    <TouchableOpacity style={styles.button}>
+      <View style={styles.container}>
+        <Text style={styles.text}>{props.id} -</Text>
+        <Text style={styles.text}>{props.nome}</Text>
+      </View>
+    </TouchableOpacity>
+    
   );
 }
 
--- src/components/Pessoa/styles.js
+++ src/components/Pessoa/styles.js
@@ -4,12 +4,23 @@ export default StyleSheet.create({
   container: {
     flexDirection: 'row',
     backgroundColor: 'rgba(125, 116, 177, 0.4)',
-    padding: 20,
-    marginTop: 20,
+    
+    justifyContent: 'center', 
+    alignItems: 'center',
   },
   text: {
     fontSize: 18,
     fontWeight: 'bold',
     color: '#FFF',
+    paddingHorizontal: 5,
+  },
+  button: {
+    backgroundColor: '#7D74B1',
+    borderWidth: 1,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    marginTop: 20,
   },
 })
