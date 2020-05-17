b342882502bfee32fd0d17fa5bbda2ecaeb89f7c
Gean-Santos <geansantos_15@hotmail.com>
2020-05-16 23:41:56 -0300

    Editing  database

--- src/database/init-database.js
+++ src/database/init-database.js
@@ -1,12 +1,31 @@
-import { DatabaseConnection } from './database-connection';
-
-let db = null;
+import { DatabaseConnection } from './connection';
+var db = DatabaseConnection.getConnection()
 export default class databaseInit {
-  db = DatabaseConnection.getConnection();
   constructor() {
     db.exec([{ sql: 'PRAGMA foreign_keys = ON;', args: [] }], false, () =>
       console.log('Foreign keys turned on')
     );
+    this.initDb();
+  }
+  initDb() {
+    var sql = [
+      `create table if not exists pessoa (id integer primary key autoincrement, nome text,
+        apelido text, telefone int);`,
+      `insert into pessoa(nome, apelido, telefone) values('Lucas', 'lu', '13997608250');`,
+    ];
+    
+    db.transaction(tx => {
+      sql.map(function(sql) {
+        console.log("execute sql : " + sql);
+        tx.executeSql(sql);
+      }) 
+    }, (error) => {
+      console.log("error call back : " + JSON.stringify(error));
+      console.log(error);
+    },
+    () => {
+      console.log("transaction complete call back ");
+    });
   }
 
 } 
