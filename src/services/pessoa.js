aa79652b1b99fa2e6d0535c4949214f2929faf0e
Gean-Santos <geansantos_15@hotmail.com>
2020-05-17 04:10:58 -0300

    Editing services

--- src/services/pessoa.js
+++ src/services/pessoa.js
@@ -5,8 +5,34 @@ const table = "pessoa";
 
 const db = DatabaseConnection.getConnection();
 
-export default function PessoaService() {
-  function addData(Pessoa) {
-    
+export default class PessoaService {
+
+
+  static addData(name, nickname, tel) {
+    return new Promise((resolve, reject) =>db.transaction(
+        tx => {
+            tx.executeSql(`insert into ${table} (nome, apelido, telefone) 
+            values (?,  ?,  ?)`, 
+            [name, nickname, tel], 
+            (_, { insertId, rows }) => {
+                console.log("id insert: " + insertId);
+                resolve(insertId)
+            }), (sqlError) => {
+                console.log(sqlError);
+            }}, (txError) => {
+            console.log(txError);
+        }));
+}
+
+    static findAll() {
+      return new Promise((resolve, reject) => db.transaction(tx => {
+          tx.executeSql(`select * from ${table}`, [], (_, { rows }) => {
+              resolve(rows)
+          }), (sqlError) => {
+              console.log(sqlError);
+          }}, (txError) => {
+          console.log(txError);
+      }))
   }
+
 }
