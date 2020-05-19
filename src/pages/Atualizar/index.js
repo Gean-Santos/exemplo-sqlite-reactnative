323ae0422ed20defbf87b444be5eb2d121dccf86
Gean-Santos <geansantos_15@hotmail.com>
2020-05-18 23:12:10 -0300

    Editing page Atualizar

--- src/pages/Atualizar/index.js
+++ src/pages/Atualizar/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
 
 import styles from './styles';
-
+const initialState = {
+  nome: '',
+  apelido: '',
+  telefone: '',
+}
 export default class Atualizar extends Component {
+  constructor(){
+    super();
+    this.state = {...initialState}
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}> Atualizar </Text>
+        <Text style={styles.title}> Atualizar o Cliente </Text>
+        <View style={styles.box}>
+          <TextInput placeholder='Digite o nome: ' />
+        </View>
       </View>
     )
   }
